65ce58ac83588db6b1ad957d821e047dab89774f
Chetan Gosavi <chetangosavi81@gmail.com>
2025-01-04 21:00:11 +0530

    javascript

---
+++ JavascriptBasics/Exercises/scope.js
@@ -0,0 +1,11 @@
+let teacher = "chetan";
+
+function manipulate(){
+    teacher = "suyog";
+    topic = "react";
+    console.log("Welcome");
+}
+
+manipulate();
+console.log(teacher);
+console.log(topic);

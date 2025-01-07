eef168b7b13b5c834c3bd4db0629f71466e71a25
Chetan Gosavi <chetangosavi81@gmail.com>
2025-01-07 16:57:26 +0530

    JavaScript

--- JavascriptBasics/Exercises/ClouserChallenges/Challenge4.js
+++ JavascriptBasics/Exercises/ClouserChallenges/Challenge4.js
@@ -4,22 +4,5 @@
 // and return that output. If it is called any additional times, instead of calling 
 // the callback again it will simply return the output value from the first time it was called.
 
-function addByX(x) {
-    return function(y){
-      console.log (x+y); }
-  }
-  
-  // /*** Uncomment these to check your work! ***/
-  const addByTwo = addByX(2);
-  addByTwo(1); // => should return 3
-  addByTwo(2); // => should return 4
-  addByTwo(3); // => should return 5
-  
-  const addByThree = addByX(3);
-  addByThree(1); // => should return 4
-  addByThree(2); // => should return 5
-  
-  const addByFour = addByX(4);
-  addByFour(4); // => should return 8
-  addByFour(5); // => should return 9
+
   
